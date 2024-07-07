27db88187b37f106c0c1b036a26ae7ca0ccde862
Blagoja Nikoloski <blagojanikoloski1@gmail.com>
2024-07-07 19:33:48 +0200

    Improved CSS

--- FiresMk.Server/obj/Debug/net8.0/FiresMk.Server.AssemblyInfo.cs
+++ FiresMk.Server/obj/Debug/net8.0/FiresMk.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FiresMk.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0df6aaad81d54df02ec47c1bbd687c38e1c31998")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7da28969f312d8d9af928ac0c1facfc5d548a4de")]
 [assembly: System.Reflection.AssemblyProductAttribute("FiresMk.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FiresMk.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
